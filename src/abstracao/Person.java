49c9dd61893f8725f23c16f699e168a7098228a4
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-05 19:15:53 -0300

    Create Person.java

---
+++ src/abstracao/Person.java
@@ -0,0 +1,19 @@
+package abstracao;
+
+public class Person {
+	private String name;
+	private int age;
+	public int getAge() {
+		return this.age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
