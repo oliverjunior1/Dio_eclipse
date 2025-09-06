f4b294e86d3cb92b05090246c6bffdcd9d43affd
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-06 17:48:33 -0300

    Update Main.java

--- src/record_java/Main.java
+++ src/record_java/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		var person = new Person("Joao");
-		System.out.println(person);
+		System.out.println(person.getInfo());
+		System.out.println(person.name());
 
 	}
 
