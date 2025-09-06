fbfe3dbe723775225863f488795a53d9aa853484
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-06 17:48:36 -0300

    Update Person.java

--- src/record_java/Person.java
+++ src/record_java/Person.java
@@ -1,5 +1,16 @@
 package record_java;
 
-public record Person(String name) {
+public record Person(String name, int age) {
 	
+	public Person{
+		
+	}
+	
+	public Person(String name) {
+		this(name,1);
+	}
+	
+	public String getInfo() {
+		return "Name: " + name + "age: " + age;
+	}
 }
