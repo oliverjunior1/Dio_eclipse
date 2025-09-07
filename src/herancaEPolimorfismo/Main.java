4ded9b594ad074d4f72bb591a2da8c6e5bf04239
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-07 17:02:51 -0300

    Create Main.java

---
+++ src/herancaEPolimorfismo/Main.java
@@ -0,0 +1,20 @@
+package herancaEPolimorfismo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+		Manager manager = new Manager();
+		
+		manager.setName("João");
+		manager.setLogin("joao");
+		manager.setPassword("123456");
+		
+		System.out.println(manager.getName());
+		System.out.println(manager.getLogin());
+		System.out.println(manager.getPassword());
+		
+		
+	}
+
+}
