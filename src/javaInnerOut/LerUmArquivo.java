40de3055bedf053272ddae75a14190b4bde4556b
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-11 17:41:51 -0300

    Update LerUmArquivo.java

--- src/javaInnerOut/LerUmArquivo.java
+++ src/javaInnerOut/LerUmArquivo.java
@@ -1,10 +1,20 @@
 package javaInnerOut;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LerUmArquivo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try (BufferedReader reader = new BufferedReader(new FileReader("saida.txt"))) {
+			String linha;
+			while((linha = reader.readLine()) != null) {
+				System.out.println(linha);
+			}
+		} catch (IOException e) {
+			System.out.println("Erro ao lser o arquivo: " + e.getMessage());
+		}
 	}
 
 }
